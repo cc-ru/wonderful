8c9f7d3d87293cff857c7c178351200aa2acfef1
Fingercomp <fingercomp@users.noreply.github.com>
2017-12-17 01:54:08 +0700

    Add a few buffer methods

--- src/buffer.lua
+++ src/buffer.lua
@@ -1,11 +1,95 @@
 local class = require("lua-objects")
 
+local floor = math.floor
+
+local function channels(color)
+  return floor(color / 0x10000),
+         floor((color % 0x10000) / 0x100),
+         color % 0x100
+end
+
 local Buffer = class(nil, {name = "wonderful.buffer.Buffer"})
+
+Buffer.defaultFg = 0xffffff
+Buffer.defaultBg = 0x000000
 
 function Buffer:__new__(args)
   self.w = args.w
   self.h = args.h
   self.depth = args.depth
+  self.cells = {}
+end
+
+function Buffer:index(x, y)
+  return self.w * (y - 1) + 3 * (x - 1) + 1
+end
+
+function Buffer:coords(index)
+  local i = (index - 1) / 3
+  local y = floor(i / self.w) + 1
+  local x = floor(i) % self.w + 1
+  return x, y
+end
+
+function Buffer:inRange(x, y)
+  return x >= 1 and x <= self.w and y >= 1 and y <= self.h
+end
+
+function Buffer:alphaBlend(color1, color2, alpha)
+  if color1 == color2 then
+    return color1
+  end
+  if alpha == 0 then
+    return color1
+  end
+  if alpha == 1 then
+    return color2
+  end
+  local r1, g1, b1 = channels(color1)
+  local r2, g2, b2 = channels(color2)
+  local ialpha = 1 - alpha
+  return floor(r1 * ialpha + r2 * alpha + 0.5) * 0x10000 +
+         floor(g1 * ialpha + g2 * alpha + 0.5) * 0x100 +
+         floor(b1 * ialpha + b2 * alpha + 0.5)
+end
+
+function Buffer:set(x, y, fg, bg, alpha, char)
+  local i = self:index(x, y)
+
+  local cfg = self.cells[i]
+  fg = self:alphaBlend(cfg, fg, alpha)
+  if fg == self.defaultFg then
+    fg = nil
+  end
+
+  local cbg = self.cells[i + 1]
+  bg = self:alphaBlend(cbg, bg, alpha)
+  if bg == self.defaultBg then
+    bg = nil
+  end
+
+  self.cells[i] = fg
+  self.cells[i + 1] = bg
+  self.cells[i + 2] = char
+end
+
+function Buffer:get(x, y)
+  local i = self:index(x, y)
+  return self.cells[i + 2], self.cells[i], self.cells[i + 1]
+end
+
+function Buffer:fill(x0, y0, w, h, fg, bg, alpha, char)
+  if fg == self.defaultFg then
+    fg = nil
+  end
+  if bg == self.defaultBg then
+    bg = nil
+  end
+  for x = x0, x0 + w - 1, 1 do
+    for y = y0, y0 + h - 1, 1 do
+      self:set(x, y, fg, bg, alpha, char)
+    end
+  end
 end
 
 return {
